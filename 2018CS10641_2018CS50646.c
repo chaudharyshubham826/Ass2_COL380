23d2eb7480a54035535ee7ba74418fdfdc45dcf3
Shubham Chaudhary <chaudharyshubham826@gmail.com>
2021-04-13 16:39:13 +0530

    some other files added

--- 2018CS10641_2018CS50646.c
+++ 2018CS10641_2018CS50646.c
@@ -12,6 +12,7 @@ void crout(double const **A, double **L, double **U, int n)
     for (i = 0; i < n; i++)
     {
         U[i][i] = 1;
+        // All diagonal entries 1 in U
     }
     for (j = 0; j < n; j++)
     {
@@ -38,6 +39,58 @@ void crout(double const **A, double **L, double **U, int n)
             U[j][i] = (A[j][i] - sum) / L[j][j];
         }
     }
+}
+
+void str_1(double const **A, double **L, double **U, int n)
+{
+    int i, j, k;
+    double sum = 0;
+
+    #pragma omp parallel for
+    for (i = 0; i < n; i++)
+    {
+        U[i][i] = 1;
+        // All diagonal entries 1 in U
+    }
+    for (j = 0; j < n; j++)
+    {
+        for (i = j; i < n; i++)
+        {
+            sum = 0;
+            for (k = 0; k < j; k++)
+            {
+                sum = sum + L[i][k] * U[k][j];
+            }
+            L[i][j] = A[i][j] - sum;
+        }
+        for (i = j; i < n; i++)
+        {
+            sum = 0;
+            for (k = 0; k < j; k++)
+            {
+                sum = sum + L[j][k] * U[k][i];
+            }
+            if (L[j][j] == 0)
+            {
+                exit(0);
+            }
+            U[j][i] = (A[j][i] - sum) / L[j][j];
+        }
+    }
+}
+
+void write_output(char fname[], double **arr, int n)
+{
+    FILE *f = fopen(fname, "w");
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            fprintf(f, "%0.12f ", arr[i][j]);
+        }
+        fprintf(f, "\n");
+    }
+    fclose(f);
 }
 
 
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Ass2_COL380
 This is the 2nd assignment for the course COL380(Parallel and distributed systems) at IITD, taught by Prof. Soham Chakraborty
+
+
+
+Strategy 1:
+First for loop, simple assig so use parallel for

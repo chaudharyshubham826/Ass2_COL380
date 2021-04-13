a5389c1d68f0bde11dcfcb1443ee2aa508c9a136
Shubham Chaudhary <chaudharyshubham826@gmail.com>
2021-04-13 12:28:43 +0530

    added c file

---
+++ 2018CS10641_2018CS50646.c
@@ -0,0 +1,52 @@
+#include <omp.h>
+
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+void crout(double const **A, double **L, double **U, int n)
+{
+    int i, j, k;
+    double sum = 0;
+    for (i = 0; i < n; i++)
+    {
+        U[i][i] = 1;
+    }
+    for (j = 0; j < n; j++)
+    {
+        for (i = j; i < n; i++)
+        {
+            sum = 0;
+            for (k = 0; k < j; k++)
+            {
+                sum = sum + L[i][k] * U[k][j];
+            }
+            L[i][j] = A[i][j] - sum;
+        }
+        for (i = j; i < n; i++)
+        {
+            sum = 0;
+            for (k = 0; k < j; k++)
+            {
+                sum = sum + L[j][k] * U[k][i];
+            }
+            if (L[j][j] == 0)
+            {
+                exit(0);
+            }
+            U[j][i] = (A[j][i] - sum) / L[j][j];
+        }
+    }
+}
+
+
+int main(int argc, char* argv[]){
+
+
+
+
+
+    return 0;
+}
